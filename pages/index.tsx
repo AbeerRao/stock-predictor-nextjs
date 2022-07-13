6b91c270b92d7a9b1e31d27c17bdc78a91816607
Abeer Rao <abeerrao@icloud.com>
2022-07-13 14:43:04 +0530

    Final deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,84 +1,107 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+
+  const [symbol, setSymbol] = useState('')
+  const [time, setTime] = useState(0)
+  const [returnData, setReturnData] = useState([])
+  const [predData, setPredData] = useState([])
+
+  const search = async () => {
+    const data = await fetch(`http://139.59.23.119/stock/${symbol}/${time}`).then(res => res.json())
+    setReturnData(data)
+  }
+
+  const predict = async () => {
+    if (returnData[0] === undefined) {
+      alert("Please search first")
+    } else {
+      const pred_data = await fetch("http://139.59.23.119/stock/pred").then(res => res.json())
+      setPredData(pred_data)
+      console.log(pred_data)
+    }
+  }
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex flex-col items-center py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Stock Predictor App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
+      <main className="flex w-full flex-1 flex-col items-center px-20 py-10 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+          <a className="text-blue-600">
+            Stock Predictor!
           </a>
         </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
-        </p>
-
-        <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and its API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      <form className="mt-5 flex flex-col space-y-4 border-2 border-gray-100 rounded-xl p-10 shadow-md">
+        <div className="flex items-center space-x-4">
+          <div className="flex items-center space-x-2">
+            <label className="text-sm font-bold text-gray-500" htmlFor="symbol">Ticker Symbol</label>
+            <input className="px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" value={symbol} onChange={(e) => setSymbol(e.target.value)} placeholder="Ticker of company" />
+          </div>
+          <div className="flex items-center space-x-2">
+            <label className="text-sm font-bold text-gray-500" htmlFor="time">Time Period</label>
+            <input className="px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" value={time} onChange={(e) => setTime(e.target.value)} placeholder="Time period ( days )"/>
+          </div>
+        </div>
+        <div className="flex items-center space-x-4">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition ease-in-out duration-200" type="button" onClick={search}>Search</button>
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition ease-in-out duration-200" type="button" onClick={predict}>Make Prediction</button>
+        </div>
+      </form>
+
+      {predData[0] !== undefined ?
+        <div>
+          <div className="mt-4 pt-5">
+            {predData[0] == 0 ?
+              <div>
+                <h1 className="text-gray-500">Prediction is that the closing stock price for tomorrow will be <span className="font-xl text-red-400 font-bold">Lower</span> than it was today </h1>
+              </div> :
+              <div>
+                <h1 className="text-gray-500">Prediction is that the closing stock price for tomorrow will be <span className="font-xl text-green-400 font-bold">Higher</span> than it was today </h1>
+              </div>
+            }
+          </div>
+          <div>
+            <h1 className="text-gray-500">Accuracy of the prediction: <span className="font-bold text-black">{predData[2]}</span></h1>
+          </div>
+        </div>
+      : null}
+
+      <table className="text-left w-2/3 shadow-xl mt-4">
+        <thead className="bg-black flex text-white w-full rounded-t-xl">
+          <tr className="flex w-full mb-4">
+            <th className="p-4 w-1/4">Date</th>
+            <th className="p-4 w-1/4">Open</th>
+            <th className="p-4 w-1/4">High</th>
+            <th className="p-4 w-1/4">Low</th>
+            <th className="p-4 w-1/4">Close</th>
+            <th className="p-4 w-1/4">Volume</th>
+          </tr>
+        </thead>
+        <tbody className="scrollbar-hide rounded-b-xl border-2 border-black bg-gray-light flex flex-col items-center justify-between overflow-y-scroll w-full h-64">
+          {returnData.map((data) => {
+            return (
+              <tr className="flex w-full mb-4 items-center">
+                <td className="p-4 w-1/4 text-gray-500 text-sm">{data['date']}</td>
+                <td className="p-4 w-1/4 text-gray-500">$ <span className="font-bold text-black">{data['open']}</span></td>
+                <td className="p-4 w-1/4 text-gray-500">$ <span className="font-bold text-black">{data['high']}</span></td>
+                <td className="p-4 w-1/4 text-gray-500">$ <span className="font-bold text-black">{data['low']}</span></td>
+                <td className="p-4 w-1/4 text-gray-500">$ <span className="font-bold text-black">{data['close']}</span></td>
+                <td className="p-4 w-1/4 text-gray-500">$ <span className="font-bold text-black">{data['volume']}</span></td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+        
     </div>
   )
 }
